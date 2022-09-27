f961804504c99e05492502a85a918c728942712e
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2022-09-28 01:07:19 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:kaangiray26/deezer-plus.git main:gh-pages
+git push origin gh-pages
 
 cd -
