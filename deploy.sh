4611912c7130ad5a504c0db4a302143f6d55c2c7
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2022-09-28 01:36:35 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd src/dist
+cd src
 
 # if you are deploying to a custom domain
 echo 'deezer.buzl.uk' > CNAME
 
-git add -A
+git add dist
 git commit -m 'deploy'
-git push -f git@github.com:kaangiray26/deezer-plus.git main:gh-pages
+git subtree push --prefix dist origin gh-pages
 
 cd -
