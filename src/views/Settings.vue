b19d2267db699a1f2094da6bc2b7646177aa79bd
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2022-12-04 12:09:16 +0100

    gave up on user agent spoofing

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -103,7 +103,7 @@
                     <hr />
                     <div class="d-flex flex-column">
                         <p>
-                            <span class="fw-bolder text-danger">Deezer+</span> is a free,
+                            <span class="fw-bolder text-danger">Deezer+</span> is a free, non-commercial
                             open-source project.<br />
                             If you like it, please consider supporting it by <a
                                 href="https://www.buymeacoffee.com/kaangiray26" target="_blank"><u>buying me a
