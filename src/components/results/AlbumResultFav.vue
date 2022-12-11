f51e41d045d7b981076c828697050c0105bdb35a
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2022-12-11 15:56:11 +0100

    fixed center column in album search

--- src/components/results/AlbumResultFav.vue
+++ src/components/results/AlbumResultFav.vue
@@ -1,14 +1,15 @@
 <template>
     <tr class="row gx-0 d-flex flex-row" :album_id="props.album.id" :album_title="props.album.title"
         :artist_id="props.artist.id" type="albums" style="flex-wrap: nowrap; width: 100% !important;">
-        <td class="col-6 text-nowrap text-truncate">
+        <td class="col-6 d-flex align-items-center text-nowrap text-truncate">
             <div>
                 <img class="img-fluid" :src="props.cover" width="40" height="40" />
-                <router-link :to="/album/ + props.album.id" class="btn btn-link track-link" @click="emit('route-click')">
+                <router-link :to="/album/ + props.album.id" class="btn btn-link track-link"
+                    @click="emit('route-click')">
                     {{ props.album.title }}</router-link>
             </div>
         </td>
-        <td class="col-3 text-nowrap text-truncate">
+        <td class="col-3 d-flex align-items-center text-nowrap text-truncate">
             <div>
                 <router-link :to="/artist/ + props.artist.id" @click="emit('route-click')">{{ props.artist.title }}
                 </router-link>
--- src/components/results/ArtistResultFav.vue
+++ src/components/results/ArtistResultFav.vue
@@ -3,16 +3,16 @@
         style="flex-wrap: nowrap; width: 100% !important;">
         <td class="col-9 d-flex align-items-center text-nowrap text-truncate">
             <div><img class="img-fluid" :src="props.cover" width="40" height="40" />
-                <router-link :to="/artist/+props.artist.id" class="btn btn-link track-link"
+                <router-link :to="/artist/ + props.artist.id" class="btn btn-link track-link"
                     @click="emit('route-click')">
-                    {{props.artist.title}}</router-link>
+                    {{ props.artist.title }}</router-link>
             </div>
         </td>
         <td class="col-1 d-flex align-items-center text-nowrap text-truncate">
-            <div><span>{{props.nb_album}}</span></div>
+            <div><span>{{ props.nb_album }}</span></div>
         </td>
         <td class="col-1 d-flex align-items-center text-nowrap text-truncate">
-            <div><span>{{props.nb_fan}}</span></div>
+            <div><span>{{ props.nb_fan }}</span></div>
         </td>
         <td class="col-1 d-flex align-items-center justify-content-end text-nowrap text-truncate">
             <button class="btn btn-light bi" :class="isFav" type="button" style="opacity: 0.90;"
